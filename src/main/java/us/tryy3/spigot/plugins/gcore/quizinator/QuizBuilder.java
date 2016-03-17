8e0866f87f81d1aee28af94403d7faf1b3e29009
tryy3 <dennistryy3@gmail.com>
2016-03-17 02:24:15 +0100

    foruth commit

---
+++ src/main/java/us/tryy3/spigot/plugins/gcore/quizinator/QuizBuilder.java
@@ -0,0 +1,11 @@
+package us.tryy3.spigot.plugins.gcore.quizinator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by tryy3 on 2016-03-17.
+ */
+public class QuizBuilder {
+    public List<QuizStep> steps = new ArrayList<>();
+}
---
+++ src/main/java/us/tryy3/spigot/plugins/gcore/quizinator/QuizListener.java
@@ -0,0 +1,7 @@
+package us.tryy3.spigot.plugins.gcore.quizinator;
+
+/**
+ * Created by tryy3 on 2016-03-17.
+ */
+public class QuizListener {
+}
